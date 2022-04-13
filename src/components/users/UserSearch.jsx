037009cda8b380600d2ae50d60519c33d65cb0ae
Ryan <sailantht@gmail.com>
2022-04-13 14:35:28 +0430

    Alert Component

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,5 +1,6 @@
 import {useState, useContext} from "react";
 import GithubContext from "../../context/github/GithubContext";
+import AlertContext from "../../context/alert/AlertContext";
 
 
 const UserSearch = () => {
@@ -7,6 +8,7 @@ const UserSearch = () => {
     // When we have a form usually a form input has its own state. component level state
 
     const {users, searchUsers, clearUsers} = useContext(GithubContext)
+    const { setAlert} = useContext(AlertContext)
 
     const handleChange = (e) => {
         setText(e.target.value)
@@ -15,7 +17,7 @@ const UserSearch = () => {
     const handleSubmit = (e) => {
         e.preventDefault()
         if (text === '') {
-            window.alert("Please enter something")
+            setAlert("Please enter something", "error")
         } else {
             searchUsers(text)
             setText('')
